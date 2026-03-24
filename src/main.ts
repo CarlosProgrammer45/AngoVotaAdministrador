447d9ed00ec146762053e5df0194f88e26a23171
Carlos <cpc417@gmail.com>
2026-03-24 16:37:25 +0100

    AtualizacaoNovas

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { provideRouter, Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { provideRouter, Router, NavigationStart, NavigationEnd, withHashLocation } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), //hash routing
     provideHttpClient()
   ]
 })
