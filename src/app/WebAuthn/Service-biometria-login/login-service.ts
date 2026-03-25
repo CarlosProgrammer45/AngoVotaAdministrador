aaa5c206cc26edb24bc88f03b2755337a961ad59
CarlosProgrammer45 <cpc417@gmail.com>
2026-03-25 21:12:44 +0100

    Update login-service.ts

--- src/app/WebAuthn/Service-biometria-login/login-service.ts
+++ src/app/WebAuthn/Service-biometria-login/login-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
-import { environment } from '../../environment/environment';
+import { environment } from '../../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
